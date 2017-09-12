e441c1ba4b5f7458edd0229a37834117607d6765
Dong Hun Yi <dhyi@Dongs-MacBook.local>
2017-09-12 18:20:55 -0400

    Initial Commit

---
+++ Project 5 - Soundboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project 5 - Soundboard
+//
+//  Created by Dong Hun Yi on 9/12/17.
+//  Copyright © 2017 PrinceYiCoding. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
